17d77faff0dcf8bddda46b91f637298bd4c67e18
meghankelly-cartography <mkelly22@wisc.edu>
2016-04-04 13:08:31 -0500

    Projection swap

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function setMap(){
 
 	//map frame dimensions
     var width = 960,
-        height = 960;
+        height = 500;
 
     //create new svg container for the map
     var map = d3.select("body")
@@ -20,11 +20,21 @@ function setMap(){
 //    		.translate([width / 2, height / 2])
 //     	.precision(.1);
 
-    var projection = d3.geo.azimuthalEqualArea()
-    	.clipAngle(180 - 1e-3)
-    	.scale(237)
-    	.translate([width / 2, height / 2])
-    	.precision(.1);
+var projection = d3.geo.robinson()
+    .scale(150)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+
+// 	var projection = d3.geo.naturalEarth()
+//     	.scale(167)
+//     	.translate([width / 2, height / 2])
+//     	.precision(.1);
+
+//     var projection = d3.geo.azimuthalEqualArea()
+//     	.clipAngle(180 - 1e-3)
+//     	.scale(237)
+//     	.translate([width / 2, height / 2])
+//     	.precision(.1);
         
     var path = d3.geo.path()
         .projection(projection);
@@ -39,17 +49,17 @@ function setMap(){
                 
     function callback(error, csvData, world){
 	   	
-	   	// graticule generator
-        var graticule = d3.geo.graticule()
-            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
-
-        //create graticule lines
-        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
+// 	   	graticule generator
+//         var graticule = d3.geo.graticule()
+//             .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+// 
+//         create graticule lines
+//         var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+//             .data(graticule.lines()) //bind graticule lines to each element to be created
+//             .enter() //create an element for each datum
+//             .append("path") //append each element to the svg as a path element
+//             .attr("class", "gratLines") //assign class for styling
+//             .attr("d", path); //project graticule lines
   
        	//translate world TopoJson
        	var worldCountries = topojson.feature(world, world.objects.collection).features;
@@ -59,12 +69,12 @@ function setMap(){
         console.log("One more test!");
 
         //add countries
-        var countries = map.selectAll(".regions")
+        var countries = map.selectAll(".countries")
             .data(worldCountries)
             .enter()
             .append("path")
             .attr("class", function(d){
-                return "regions " + d.properties.geounit;
+                return "countries " + d.properties.geounit;
             })
             .attr("d", path);
 
