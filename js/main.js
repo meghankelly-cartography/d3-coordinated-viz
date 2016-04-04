d8bd39eec9097c614b976fb6231e32769e55c104
meghankelly-cartography <mkelly22@wisc.edu>
2016-04-04 10:18:29 -0500

    Module 8, Callback errors and projections

--- js/main.js
+++ js/main.js
@@ -2,24 +2,52 @@ window.onload = setMap();
 
 //set up choropleth map
 function setMap(){
+
+//map frame dimensions
+//     var width = 960,
+//         height = 460;
+// 
+//     //create new svg container for the map
+//     var map = d3.select("body")
+//         .append("svg")
+//         .attr("class", "map")
+//         .attr("width", width)
+//         .attr("height", height);
+// 
+//     //create Albers equal area conic projection centered on France
+//     var projection = d3.geo.albers()
+//         .center([0, 0])
+//         .rotate([-2, 0, 0])
+//         .parallels([45, 45])
+//         .scale(2500)
+//         .translate([width / 2, height / 2]);
+//         
+//     var path = d3.geo.path()
+//         .projection(projection);
+
 	//use queue.js to load data simultaneously
 	d3_queue.queue()
-        .defer(d3.csv, "data/Countries_vac.csv") //load attributes from csv
-        .defer(d3.topojson, "data/worldData.topojson") //load choropleth spatial data
+        .defer(d3.csv, "data/Countries_vac2.csv") //load attributes from csv
+        .defer(d3.json, "data/mapbase.topojson") //load background spatial data
+		.defer(d3.json, "data/map.topojson") //load choropleth spatial data
         .await(callback);
 	
 	console.log("Meghan");
                 
-    function callback(error, csvData, world){
-       console.log("Meghan");
-       console.log(error);
-	   console.log(csvData);
-	   console.log(world);
+    function callback(error, csvData, base, world){
+       	console.log("Meghan");
+      	console.log(error);
+	   	console.log(csvData);
+	   	console.log(world);
   
-    	console.log("Meghan");
-        //translate world TopoJson
-        var worldCountries = topojson.feature(world, world.objects.worldCountries).features;
+       	//translate world TopoJson
+       	var baseMap = topojson.feature(base, base.objects.Countries_base),
+			worldCountries = topojson.feature(world, world.objects.Countries).features;
 
+		console.log(baseMap);
         console.log(worldCountries);
+        
+        console.log("One more test!");
+
     };        
 };
