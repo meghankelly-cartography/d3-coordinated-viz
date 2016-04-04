28c8e7be64314e82d8bcadcd2e0042c1b6fa3825
meghankelly-cartography <mkelly22@wisc.edu>
2016-04-04 12:08:22 -0500

    Module 8 assignment completed!

--- js/main.js
+++ js/main.js
@@ -3,51 +3,70 @@ window.onload = setMap();
 //set up choropleth map
 function setMap(){
 
-//map frame dimensions
-//     var width = 960,
-//         height = 460;
-// 
-//     //create new svg container for the map
-//     var map = d3.select("body")
-//         .append("svg")
-//         .attr("class", "map")
-//         .attr("width", width)
-//         .attr("height", height);
-// 
-//     //create Albers equal area conic projection centered on France
-//     var projection = d3.geo.albers()
-//         .center([0, 0])
-//         .rotate([-2, 0, 0])
-//         .parallels([45, 45])
-//         .scale(2500)
-//         .translate([width / 2, height / 2]);
-//         
-//     var path = d3.geo.path()
-//         .projection(projection);
+	//map frame dimensions
+    var width = 960,
+        height = 960;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+        
+// Projection Option 1
+//     var projection = d3.geo.cylindricalEqualArea()
+//     	.scale(153)
+//    		.translate([width / 2, height / 2])
+//     	.precision(.1);
+
+    var projection = d3.geo.azimuthalEqualArea()
+    	.clipAngle(180 - 1e-3)
+    	.scale(237)
+    	.translate([width / 2, height / 2])
+    	.precision(.1);
+        
+    var path = d3.geo.path()
+        .projection(projection);
 
 	//use queue.js to load data simultaneously
 	d3_queue.queue()
         .defer(d3.csv, "data/Countries_vac2.csv") //load attributes from csv
-        .defer(d3.json, "data/mapbase.topojson") //load background spatial data
 		.defer(d3.json, "data/map.topojson") //load choropleth spatial data
         .await(callback);
 	
 	console.log("Meghan");
                 
-    function callback(error, csvData, base, world){
-       	console.log("Meghan");
-      	console.log(error);
-	   	console.log(csvData);
-	   	console.log(world);
+    function callback(error, csvData, world){
+	   	
+	   	// graticule generator
+        var graticule = d3.geo.graticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
   
        	//translate world TopoJson
-       	var baseMap = topojson.feature(base, base.objects.Countries_base),
-			worldCountries = topojson.feature(world, world.objects.Countries).features;
+       	var worldCountries = topojson.feature(world, world.objects.collection).features;
 
-		console.log(baseMap);
         console.log(worldCountries);
         
         console.log("One more test!");
+
+        //add countries
+        var countries = map.selectAll(".regions")
+            .data(worldCountries)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.geounit;
+            })
+            .attr("d", path);
 
     };        
 };
